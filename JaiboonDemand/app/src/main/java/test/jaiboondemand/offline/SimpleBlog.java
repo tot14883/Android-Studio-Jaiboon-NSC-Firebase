9a324e1509da5f1af5b4b87eb52b6380b6972409
napat  srichantapong <tot14883@gmail.com>
2017-12-10 00:58:47 +0700

    New

---
+++ JaiboonDemand/app/src/main/java/test/jaiboondemand/offline/SimpleBlog.java
@@ -0,0 +1,27 @@
+package test.jaiboondemand.offline;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.OkHttpDownloader;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by User on 12/8/2017.
+ */
+
+public class SimpleBlog extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(false);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+    }
+}
