a7f9dee51bf8ee8a9df5d7a9ea3d23dfe8545fac
napat  srichantapong <tot14883@gmail.com>
2017-12-28 22:01:40 +0700

    New

---
+++ JaiboonDemand/app/src/main/java/test/jaiboondemand/Pay/PayActivity.java
@@ -0,0 +1,15 @@
+package test.jaiboondemand.Pay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import test.jaiboondemand.R;
+
+public class PayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
