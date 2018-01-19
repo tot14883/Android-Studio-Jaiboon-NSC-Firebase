d9948a8dbb52f40d758b17979593ba6134d255aa
napat  srichantapong <tot14883@gmail.com>
2018-01-19 17:07:56 +0700

    new code

---
+++ JaiboonDemand/app/src/main/java/test/jaiboondemand/ChooseDonate/ChooseDonate.java
@@ -0,0 +1,23 @@
+package test.jaiboondemand.ChooseDonate;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+
+import test.jaiboondemand.R;
+
+public class ChooseDonate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_donate2);
+
+    }
+}
